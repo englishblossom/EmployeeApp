441c35eb16fa480dde5950bdd70b697e7fd6fd96
englishblossom <31792129+englishblossom@users.noreply.github.com>
2019-05-10 13:03:20 +0530

    Update EmployeeService.java

--- src/main/java/com/aishwarya/service/EmployeeService.java
+++ src/main/java/com/aishwarya/service/EmployeeService.java
@@ -7,7 +7,5 @@ import com.aishwarya.bean.Employee;
 public interface EmployeeService {
  public List<Employee> getEmployees();
  public Employee getEmployee(Long employeeId);
-// public int deleteEmployee(Long employeeId); 
-// public int updateEmployee(Employee employee);
  public int createEmployee(Employee employee); 
 }
